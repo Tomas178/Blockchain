4a3d9eeb3b2c4be79b510cdd263f1952826ae7f8
Tomas Petronis <tomaspetronis77gmail.com>
2024-09-24 17:42:55 +0300

    Po biski

--- funkcijos.cpp
+++ funkcijos.cpp
@@ -16,20 +16,49 @@ string Failo_apdorojimas(string failo_pavadinimas){
     return failo_pavadinimas;
 }
 
+string BinaryToHex(const string& binary){
+    stringstream HEX;
+    unsigned long long int HexValue = 0;
+
+    for (size_t i = 0; i < binary.size(); i += 4) {
+        string binaryChunk = binary.substr(i, 4); 
+        HexValue = bitset<4>(binaryChunk).to_ulong();   
+        HEX << hex << HexValue;                  
+    }
+
+    return HEX.str();
+}
+
+void PaddedInput(string& simboliu_seka){
+    simboliu_seka += "11111";
+    if(simboliu_seka.length() % 512 != 448){
+        for(int i = 0; i < 448 - (simboliu_seka.length() % 512); i++){
+            simboliu_seka += "0";
+        }
+    }
+}
+
+
 string Maisos_funkcija(string simboliu_seka){
 
     const unsigned int size = simboliu_seka.length();
-    bitset<8> bit_map[size];
-
+    vector<bitset<8>> bit_map(size);
     string nauja_simboliu_seka;
-    for(int i = 0; i < size; i++){
+
+    for (unsigned int i = 0; i < size; i++) {
         bit_map[i] = simboliu_seka[i];
         nauja_simboliu_seka += bit_map[i].to_string();
     }
-    nauja_simboliu_seka += "11111";
+    //nauja_simboliu_seka += "11111";
+
+    PaddedInput(nauja_simboliu_seka);
+    cout << nauja_simboliu_seka << endl;
+
+    string HexString = BinaryToHex(nauja_simboliu_seka);
 
     cout << "Bitu skaicius: "<< nauja_simboliu_seka.size() << endl;
-    cout << nauja_simboliu_seka << endl;
+    cout << HexString << endl;
+
 
     //bitset<32> bit_map[simboliu_seka.length()];
     //bitset<32> copy;
--- funkcijos.h
+++ funkcijos.h
@@ -5,11 +5,14 @@
 #include <bitset>
 #include <sstream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
 string Maisos_funkcija(string simboliu_seka);
 string Failo_apdorojimas(string failo_pavadinimas);
+string BinaryToHex(const string& binary);
+void PaddedInput(string& simboliu_seka);
 
 extern int norimas_programos_veikimas;
 extern string simboliu_seka;
